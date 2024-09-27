0fa6345ca902803922c6a45b22e8ab6f6a56972b
dushimec <dushimec515@gmail.com>
2024-09-27 17:21:00 +0200

    modified

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,11 @@
-
-
 import React from 'react';
 import { createRoot } from 'react-dom/client'; 
-import { Provider } from 'react-redux';
-import { I18nextProvider } from 'react-i18next';
 import App from './App';
 import './index.css';
-import i18n from './i18n';
 
-const root = createRoot(document.getElementById('root')); 
-root.render(
-  <Provider store={store}>
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
-  </Provider>
-);
+const renderApp = () => {
+  const root = createRoot(document.getElementById('root')); 
+  root.render(<App />);
+};
+
+export default renderApp;
