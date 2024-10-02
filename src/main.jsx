dfd54a84d1a18a690b4f0865ae8fa3ee7a07c63a
Munezerojosue <josuemunezero2@gmal.com>
2024-10-02 17:30:48 +0200

    modified

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'; 
-import App from './App'; 
+import { Provider } from 'react-redux';
+import { I18nextProvider } from 'react-i18next';
+import App from './App';
 import './index.css';
 
-const rootElement = document.getElementById('root');
-
-  const root = createRoot(rootElement);
-  root.render(<App />);
+const root = createRoot(document.getElementById('root')); 
+root.render(
+  <Provider store={store}>
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </Provider>
+);
