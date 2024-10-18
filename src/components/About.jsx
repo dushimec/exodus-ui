40c8e099de2bee062ea38e77c82919efb3dff307
kevine00 <kevineumubyeyi9@gmail.com>
2024-10-19 00:09:28 +1300

    updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,74 +3,98 @@ import React from "react";
 function About() {
   return (
 
-    <div className="flex overflow-hidden flex-col items-center bg-stone-50 bg-opacity-50">
-      <div className="flex relative flex-col self-stretch w-full text-4xl font-semibold text-white min-h-[300px] sm:min-h-[400px] md:min-h-[490px]">
+
+    <div className="flex overflow-hidden flex-col items-center  bg-stone-50 bg-opacity-50 max-md:pb-24">
+      <div className="flex relative flex-col self-stretch w-full text-4xl font-semibold text-white min-h-[490px] max-md:max-w-full">
+
         <img
           loading="lazy"
-          srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/daa3fd74395d0306601657a9c8c5a5e15532b170c14c185a02e3905b98b13927?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/daa3fd74395d0306601657a9c8c5a5e15532b170c14c185a02e3905b98b13927?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/daa3fd74395d0306601657a9c8c5a5e15532b170c14c185a02e3905b98b13927?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/daa3fd74395d0306601657a9c8c5a5e15532b170c14c185a02e3905b98b13927?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/daa3fd74395d0306601657a9c8c5a5e15532b170c14c185a02e3905b98b13927?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=1200 1200w"
-          className="object-cover absolute inset-0 w-full h-full"
-          alt="Background"
+          srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/daa3fd74395d0306601657a9c8c5a5e15532b170c14c185a02e3905b98b13927?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/daa3fd74395d0306601657a9c8c5a5e15532b170c14c185a02e3905b98b13927?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/daa3fd74395d0306601657a9c8c5a5e15532b170c14c185a02e3905b98b13927?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/daa3fd74395d0306601657a9c8c5a5e15532b170c14c185a02e3905b98b13927?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/daa3fd74395d0306601657a9c8c5a5e15532b170c14c185a02e3905b98b13927?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/daa3fd74395d0306601657a9c8c5a5e15532b170c14c185a02e3905b98b13927?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/daa3fd74395d0306601657a9c8c5a5e15532b170c14c185a02e3905b98b13927?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/daa3fd74395d0306601657a9c8c5a5e15532b170c14c185a02e3905b98b13927?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497"
+          className="object-cover absolute inset-0 size-full"
         />
-        <div className="absolute inset-0 bg-black opacity-60"></div>
-        <div className="flex relative flex-col justify-start items-center px-4 pt-16 w-full sm:px-8 sm:pt-20 md:px-20 md:pt-24">
-          <div className="px-4 py-4  mt-16 sm:px-8 sm:py-6 md:px-12 md:py-9 mb-0 text-center bg-transparent border-white border-solid bg-opacity-0 border-4 sm:border-8 md:border-[10px] w-[80%] max-w-[496px]">
+        <div className="flex relative flex-col justify-center items-center px-20 py-52 w-full bg-black bg-opacity-60 max-md:px-5 max-md:py-24 max-md:max-w-full">
+          <div className="px-36 py-9 -mb-9  max-w-full border-white border-solid bg-zinc-300 bg-opacity-0 border-[10px] w-[496px] max-md:px-5 max-md:mb-2.5">
+
             About Us
           </div>
         </div>
       </div>
-      <div className="mt-8 sm:mt-12 md:mt-20 px-4 sm:px-6 md:px-8 w-full max-w-[1232px]">
-        <div className="flex flex-col md:flex-row gap-5">
-          <div className="flex flex-col w-full md:w-6/12">
-            <div className="flex flex-col w-full">
-              <div className="self-start text-lg sm:text-xl md:text-2xl font-semibold">
-                <span className="text-2xl font-bold text-sky-500">"</span>{" "}
-                <span>
-                  OLD Fox Company offers expertly planned religious trips, guiding individuals and groups to some of the most sacred destinations worldwide. Our pilgrimages are designed to inspire spiritual growth, combining prayer, reflection, and cultural exploration. Whether visiting ancient temples or holy cities, each trip provides a meaningful opportunity for travelers to connect deeply with their faith and traditions. We ensure every journey is thoughtfully organized for a seamless and enriching experience.
+      <div className="mt-20 ml-8 w-full max-w-[1232px] max-md:mt-10 max-md:max-w-full">
+        <div className="flex gap-5 max-md:flex-col">
+          <div className="flex flex-col w-6/12 max-md:ml-0 max-md:w-full">
+            <div className="flex flex-col w-full max-md:max-w-full">
+              <div className="self-start ml-8 text-3xl font-semibold max-md:max-w-full">
+                <span className="text-4xl font-bold text-sky-500">“</span>{" "}
+                <span className="text-2xl">
+                  OLD Fox Company offers faith-based global travel experiences,
+                  connecting people through  meaningful religious journeys
+
                 </span>{" "}
-                <span className="text-2xl font-bold text-sky-500">"</span>
+                <span className="text-4xl font-bold text-sky-500">”</span>
               </div>
-              <div className="mt-8 sm:mt-12 md:mt-28">
-                <div className="flex flex-col items-center">
-                  <img
-                    loading="lazy"
-                    srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/1eaa0b4cc783d97d709ba024250dfc911bf2da9a4d4c0dcc052f4a967570edcc?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/1eaa0b4cc783d97d709ba024250dfc911bf2da9a4d4c0dcc052f4a967570edcc?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/1eaa0b4cc783d97d709ba024250dfc911bf2da9a4d4c0dcc052f4a967570edcc?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/1eaa0b4cc783d97d709ba024250dfc911bf2da9a4d4c0dcc052f4a967570edcc?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/1eaa0b4cc783d97d709ba024250dfc911bf2da9a4d4c0dcc052f4a967570edcc?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/1eaa0b4cc783d97d709ba024250dfc911bf2da9a4d4c0dcc052f4a967570edcc?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/1eaa0b4cc783d97d709ba024250dfc911bf2da9a4d4c0dcc052f4a967570edcc?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=2000 2000w"
-                    className="object-contain z-0 w-full aspect-[1.74] shadow-[0px_100px_80px_rgba(0,0,0,0.07)]"
-                    alt="Religious destination"
-                  />
+              <div className="mt-7 max-md:max-w-full">
+                <div className="flex gap-5 max-md:flex-col">
+                <div className="flex flex-col w-full max-w-[73%] mx-auto max-md:w-full mt-[80px] max-md:mt-[60px]">
+  <img
+    loading="lazy"
+    srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/1eaa0b4cc783d97d709ba024250dfc911bf2da9a4d4c0dcc052f4a967570edcc?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=100 100w,
+      https://cdn.builder.io/api/v1/image/assets/TEMP/1eaa0b4cc783d97d709ba024250dfc911bf2da9a4d4c0dcc052f4a967570edcc?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=200 200w,
+      https://cdn.builder.io/api/v1/image/assets/TEMP/1eaa0b4cc783d97d709ba024250dfc911bf2da9a4d4c0dcc052f4a967570edcc?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=400 400w,
+      https://cdn.builder.io/api/v1/image/assets/TEMP/1eaa0b4cc783d97d709ba024250dfc911bf2da9a4d4c0dcc052f4a967570edcc?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=800 800w,
+      https://cdn.builder.io/api/v1/image/assets/TEMP/1eaa0b4cc783d97d709ba024250dfc911bf2da9a4d4c0dcc052f4a967570edcc?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=1200 1200w,
+      https://cdn.builder.io/api/v1/image/assets/TEMP/1eaa0b4cc783d97d709ba024250dfc911bf2da9a4d4c0dcc052f4a967570edcc?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=1600 1600w,
+      https://cdn.builder.io/api/v1/image/assets/TEMP/1eaa0b4cc783d97d709ba024250dfc911bf2da9a4d4c0dcc052f4a967570edcc?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=2000 2000w"
+    className="object-contain z-0 grow mt-0 w-full aspect-[1.74] shadow-[0px_100px_80px_rgba(0,0,0,0.07)]"
+  />
+</div>
+
+                  <div className="flex flex-col ml-5 w-[27%] max-md:ml-0 max-md:w-full">
+                    <img
+                    
+                      loading="lazy"
+                      srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/ed8be69ecaf0511501a26101139e6efd5b4c6503178bc2cab5d0beb1078e74ef?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/ed8be69ecaf0511501a26101139e6efd5b4c6503178bc2cab5d0beb1078e74ef?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/ed8be69ecaf0511501a26101139e6efd5b4c6503178bc2cab5d0beb1078e74ef?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/ed8be69ecaf0511501a26101139e6efd5b4c6503178bc2cab5d0beb1078e74ef?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/ed8be69ecaf0511501a26101139e6efd5b4c6503178bc2cab5d0beb1078e74ef?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/ed8be69ecaf0511501a26101139e6efd5b4c6503178bc2cab5d0beb1078e74ef?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/ed8be69ecaf0511501a26101139e6efd5b4c6503178bc2cab5d0beb1078e74ef?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/ed8be69ecaf0511501a26101139e6efd5b4c6503178bc2cab5d0beb1078e74ef?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497"
+                      className="object-contain shrink-0 max-w-full aspect-square w-[200px]"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-
-          <div className="flex flex-col w-full md:w-6/12 mt-8 md:mt-0">
-            <div className="flex flex-col text-lg sm:text-xl md:text-2xl font-semibold text-black">
+          <div className="flex flex-col ml-5 w-6/12 max-md:ml-0 max-md:w-full">
+            <div className="flex flex-col mt-1.5 text-3xl font-semibold text-black max-md:max-w-full">
               <img
                 loading="lazy"
-                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/edebd2491ac257f0bdbfa7397524775755b9a289ebba783ff89819da11b193d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/edebd2491ac257f0bdbfa7397524775755b9a289ebba783ff89819da11b193d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/edebd2491ac257f0bdbfa7397524775755b9a289ebba783ff89819da11b193d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/edebd2491ac257f0bdbfa7397524775755b9a289ebba783ff89819da11b193d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/edebd2491ac257f0bdbfa7397524775755b9a289ebba783ff89819da11b193d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/edebd2491ac257f0bdbfa7397524775755b9a289ebba783ff89819da11b193d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/edebd2491ac257f0bdbfa7397524775755b9a289ebba783ff89819da11b193d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=2000 2000w"
-                className="object-contain max-w-full aspect-[1.58] shadow-[0px_100px_80px_rgba(0,0,0,0.07)] w-full"
-                alt="Religious landmark"
+                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/edebd2491ac257f0bdbfa7397524775755b9a289ebba783ff89819da11b193d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/edebd2491ac257f0bdbfa7397524775755b9a289ebba783ff89819da11b193d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/edebd2491ac257f0bdbfa7397524775755b9a289ebba783ff89819da11b193d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/edebd2491ac257f0bdbfa7397524775755b9a289ebba783ff89819da11b193d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/edebd2491ac257f0bdbfa7397524775755b9a289ebba783ff89819da11b193d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/edebd2491ac257f0bdbfa7397524775755b9a289ebba783ff89819da11b193d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/edebd2491ac257f0bdbfa7397524775755b9a289ebba783ff89819da11b193d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/edebd2491ac257f0bdbfa7397524775755b9a289ebba783ff89819da11b193d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497"
+                className="object-contain max-w-full aspect-[1.58] shadow-[0px_100px_80px_rgba(0,0,0,0.07)] w-[614px]"
               />
-              <div className="self-end pt-20 sm:mt-12  md:mt-16">
-                <span className="text-2xl font-bold text-sky-500">"</span>
-                <span > 
-                  Our itineraries create transformative experiences, blending spiritual enrichment with cultural immersion. Travelers explore significant religious landmarks, engage with local traditions, and connect with fellow believers. These journeys inspire renewal and reflection, offering a deeper understanding of faith and lasting memories in some of the world's most revered spiritual places.
+              <div className="self-end mt-6 max-md:max-w-full">
+                <span className="text-4xl font-bold text-sky-500">“</span>
+                <span className="text-2xl">
+                  {" "}
+                  We provides spiritually-focused travel services worldwide,
+                  uniting people through enriching religious adventures
                 </span>
                 <span className="font-bold">.</span>{" "}
-                <span className="text-2xl font-bold text-sky-500">"</span>
+                <span className="text-4xl font-bold text-sky-500">”</span>
               </div>
             </div>
           </div>
-          
         </div>
       </div>
      
-      <div className="flex flex-col items-center mt-12 sm:mt-16 md:mt-20">
-        <div className="h-1 w-16 border-4 border-sky-500 border-solid mb-4" />
-        <div className="text-2xl font-[inria-serif] text-sky-500">
-          Our Team
-        </div>
-        <div className="h-1 w-16 border-4 border-sky-500 border-solid mt-4 mb-8" />
+      <div className="shrink-0 mt-2 mr-32 h-1 border-4 border-sky-500 border-solid w-[62px]" />
+      <div className="mt-6  text-2xl font-[inria-serif] text-sky-500 max-md:text-3xl">
+        Our Team
       </div>
+      <div className="shrink-0 mt-4 ml-28 h-1 border-4 border-sky-500 border-solid w-[62px]" />
+      <img
+        loading="lazy"
+        srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/4870b734b0f9cf58ac339c1438f2ecdaef768164e4946c48bc073492c23539d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/4870b734b0f9cf58ac339c1438f2ecdaef768164e4946c48bc073492c23539d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/4870b734b0f9cf58ac339c1438f2ecdaef768164e4946c48bc073492c23539d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/4870b734b0f9cf58ac339c1438f2ecdaef768164e4946c48bc073492c23539d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/4870b734b0f9cf58ac339c1438f2ecdaef768164e4946c48bc073492c23539d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/4870b734b0f9cf58ac339c1438f2ecdaef768164e4946c48bc073492c23539d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/4870b734b0f9cf58ac339c1438f2ecdaef768164e4946c48bc073492c23539d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/4870b734b0f9cf58ac339c1438f2ecdaef768164e4946c48bc073492c23539d7?placeholderIfAbsent=true&apiKey=ad4b702f1041452291688c39b1990497"
+        className="object-contain mt-5 ml-5 max-w-full aspect-[0.78] shadow-[0px_100px_80px_rgba(0,0,0,0.07)] w-[285px]"
+      />
+      <div className="mt-5 text-2xl mb-10 font-[inria-serif] text-sky-500 max-md:text-4xl">
+        Ceo of Oldfox
+
 
 
       <div className="flex flex-col items-center mb-12">
@@ -170,7 +194,10 @@ function About() {
       />
       <div className="mt-5 text-2xl mb-10 font-[inria-serif] text-sky-500 max-md:text-4xl">
         Ceo of Oldfox
+
+
       </div>
+    </div>
     </div>
     </div>
     </div>
