42c069f5de179110f07603245cc4da1145868171
dushimec <dushimec515@gmail.com>
2025-02-21 10:13:43 -0800

    updates

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -70,37 +70,49 @@ function Navigation() {
 
   return (
     <>
+      
       <div
         className="fixed top-0 left-0 w-full bg-sky-600 text-white py-2 px-4 md:px-6 lg:px-10 xl:px-12 z-50"
       >
         <div className="flex flex-wrap justify-between items-center">
-          <div className="flex flex-wrap items-center space-x-4">
-            <div className="flex items-center">
-            <Timer className="h-4 w-4 mr-2" />
+          <div className="flex flex-wrap items-center space-x-4 overflow-hidden">
+            <div className="flex items-center animate-marquee whitespace-nowrap gap-5">
+              <Timer className="h-4 w-4 mr-2" />
               <span className="text-sm">Open At: 9:00 AM - 6:00 PM</span>
-            </div>
-            <div className="hidden md:flex items-center">
-              <div>|</div>
-              <MapPin className="h-4 w-4 mr-2" />
-              <span className="text-sm">KG 11 Ave Remera Gisimenti, Kigali, Rwanda</span>
-            </div>
-            <div className="hidden md:flex items-center">
-              <div>|</div>
-              
-              <Mail className="h-4 w-4 mr-2" />
-              <span className="text-sm">Oldfoxcoltd@gmail.com</span>
-            </div>
-            <div className="hidden md:flex items-center">
-              <div>|</div>
-              <span className="text-sm">OLDFOX - TOUR: <b>Local & International</b></span>
-            </div>
-          </div>
-          <div className="flex items-center space-x-4">
+              <div className="flex items-center space-x-4">
+                <div> | </div>
+                <MapPin className="h-4 w-4 mr-2" />
+                <span className="text-sm">KG 11 Ave Remera Gisimenti, Kigali, Rwanda</span>
+              </div>
+              <div className="flex items-center space-x-4">
+                <div> | </div>
+                <Mail className="h-4 w-4 mr-2" />
+                <span className="text-sm">Oldfoxcoltd@gmail.com</span>
+              </div>
+              <div className="flex items-center space-x-4">
+                <div> | </div>
+                <span className="text-sm">OLDFOX - TOUR: <b>Local & International</b></span>
+              </div>
+              <div> | </div>
+              <div className="flex items-center space-x-4">
             <PhoneCallIcon className="h-4 w-4 mr-2" />
-            <span className="hidden md:inline text-sm">+250788726181</span>
+            <span className="text-sm">+250788726181</span>
           </div>
+            </div>
+          </div>
+          
         </div>
       </div>
+
+      <style jsx>{`
+        @keyframes marquee {
+          0% { transform: translateX(100%); }
+          100% { transform: translateX(-100%); }
+        }
+        .animate-marquee {
+          animation: marquee 50s linear infinite;
+        }
+      `}</style>
 
       <nav
         className={`fixed top-[40px] left-0 flex flex-wrap items-center w-full z-40 transition-all duration-300 ${
